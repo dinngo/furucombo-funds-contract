b02f754d9c5b8da085b7b93e70e8f449cf5d11f9
benhuang <ben@dinngo.co>
2021-10-28 18:19:29 +0800

    Implement link list for asset

--- contracts/modules/AssetModule.sol
+++ contracts/modules/AssetModule.sol
@@ -5,18 +5,55 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ModuleBase} from "./ModuleBase.sol";
 
 abstract contract AssetModule is ModuleBase {
-    IERC20[] private _assetList;
+    mapping(address => address) private _predecessor;
+    mapping(address => address) private _successor;
+    uint256 private _assetCount;
 
     function getAssetValue() public view returns (uint256 assetValue) {
         this;
         return 0;
     }
 
-    function getAssetList() public view returns (IERC20[] memory assetList) {
-        return _assetList;
+    function getAssetList() public view returns (address[] memory assetList) {
+        assetList = new address[](_assetCount);
+        uint256 index = 0;
+        for (
+            address p = _successor[address(0)];
+            _successor[p] != address(0);
+            p = _successor[p]
+        ) {
+            assetList[0] = p;
+            index++;
+        }
     }
 
     function getReserve() public view returns (uint256) {
         return denomination.balanceOf(address(vault));
+    }
+
+    function addAsset(address asset) public {
+        // Should check asset value exists
+        if (_assetCount == 0) {
+            _predecessor[address(0)] = asset;
+            _successor[address(0)] = asset;
+        } else {
+            address tail = _predecessor[address(0)];
+            _successor[tail] = asset;
+            _predecessor[asset] = tail;
+            _predecessor[address(0)] = asset;
+        }
+        _assetCount++;
+    }
+
+    function removeAsset(address asset) public {
+        // Should check asset value zero
+        if (_assetCount == 0) {
+            revert("List empty");
+        } else {
+            address succ = _successor[asset];
+            address pred = _predecessor[asset];
+            _successor[pred] = succ;
+            _assetCount--;
+        }
     }
 }
