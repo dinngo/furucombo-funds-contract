4da71d3fd550969ee762ecb2f650050cae7fdcea
Albert Lin <albert@dinngo.co>
2021-12-14 11:27:45 +0800

    rebase from develop

---
+++ contracts/assets/interfaces/IAssetRouter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+import "./IAssetRegistry.sol";
+import "./IAssetOracle.sol";
+
+pragma solidity ^0.8.0;
+
+interface IAssetRouter {
+    function oracle() external view returns (IAssetOracle);
+
+    function registry() external view returns (IAssetRegistry);
+
+    function setOracle(address oracle) external;
+
+    function setRegistry(address registry) external;
+
+    function calcAssetsTotalValue(
+        address[] calldata bases,
+        uint256[] calldata amounts,
+        address quote
+    ) external view returns (uint256);
+}
