c41be1c4051a5bc8c262ac02cad5c6beaa445e7e
Albert Lin <albert@dinngo.co>
2022-01-06 15:46:36 +0800

    fix comptroller test

--- test/Comptroller.test.ts
+++ test/Comptroller.test.ts
@@ -47,14 +47,13 @@ describe('Comptroller', function () {
       ).deploy(oracle.address, registry.address);
       await assetRouter.deployed();
 
-      const execFeePercentage = 200; // 20%
       comptroller = await (
         await ethers.getContractFactory('Comptroller')
       ).deploy(
         implementation.address,
         assetRouter.address,
         collector.address,
-        execFeePercentage
+        0
       );
       await comptroller.deployed();
 
