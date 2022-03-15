3575f8cdf0a75bf5bec4a279edcacc43cce87671
benhuang <ben@dinngo.co>
2022-03-15 17:44:58 +0800

    Update deploy script

--- deploy/ComptrollerPostSetup.ts
+++ deploy/ComptrollerPostSetup.ts
@@ -7,28 +7,30 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   console.log('executing "ComptrollerPostSetup"');
 
   const { deployments, ethers } = hre;
-  const comptrollerAddress = (await deployments.get('Comptroller')).address;
-  const comptroller = await ethers.getContractAt(
-    'Comptroller',
+  const comptrollerAddress = (
+    await deployments.get('ComptrollerImplementation')
+  ).address;
+  const comptrollerImplementation = await ethers.getContractAt(
+    'ComptrollerImplementation',
     comptrollerAddress
   );
 
   // Set task executor
-  const execAction = await comptroller.execAction();
+  const execAction = await comptrollerImplementation.execAction();
   if (execAction === constants.AddressZero) {
     const taskExecutor = await deployments.get('TaskExecutor');
-    await comptroller.setExecAction(taskExecutor.address);
+    await comptrollerImplementation.setExecAction(taskExecutor.address);
   }
 
   // Permit delegate call
   const aFurucombo = await deployments.get('AFurucombo');
-  const canCall = await comptroller.canDelegateCall(
+  const canCall = await comptrollerImplementation.canDelegateCall(
     LEVEL,
     aFurucombo.address,
     WL_ANY_SIG
   );
   if (!canCall) {
-    await comptroller.permitDelegateCalls(
+    await comptrollerImplementation.permitDelegateCalls(
       LEVEL,
       [aFurucombo.address],
       [WL_ANY_SIG]
@@ -39,4 +41,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 export default func;
 
 func.tags = ['ComptrollerPostSetup'];
-func.dependencies = ['Comptroller', 'TaskExecutor', 'AFurucombo'];
+func.dependencies = ['ComptrollerImplementation', 'TaskExecutor', 'AFurucombo'];
