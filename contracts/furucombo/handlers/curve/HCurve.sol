6354d3cc032267408b1d0ee61c92db0bbd6bf1b7
Mussina Lin <mussina@dinngo.co>
2022-03-23 11:55:13 +0800

    feat: delete unused file

--- contracts/furucombo/handlers/curve/HCurve.sol
+++ contracts/furucombo/handlers/curve/HCurve.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../HandlerBase.sol";
-import "./ICurveHandler.sol";
+import {ICurveHandler} from "../../../interfaces/ICurveHandler.sol";
 
 contract HCurve is HandlerBase {
     using SafeERC20 for IERC20;
