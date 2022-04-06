d12f796a81b7db1a818f6aee372d2a608b2e5110
benhuang <ben@dinngo.co>
2022-04-06 15:49:21 +0800

    Add price change testcases

--- test/PerformanceFee.test.ts
+++ test/PerformanceFee.test.ts
@@ -2,8 +2,8 @@ import { Wallet, BigNumber } from 'ethers';
 import { expect } from 'chai';
 import { ethers, deployments } from 'hardhat';
 import { PerformanceFeeModuleMock, ShareToken } from '../typechain';
-import { FUND_PERCENTAGE_BASE } from './utils/constants';
-import { get64x64FromNumber, expectEqWithinBps } from './utils/utils';
+import { DS_PROXY_REGISTRY, FUND_PERCENTAGE_BASE } from './utils/constants';
+import { get64x64FromNumber, ether, expectEqWithinBps } from './utils/utils';
 
 /// @notice increase the block time need mine block,
 /// the next view function will be correct.
@@ -226,40 +226,263 @@ describe('Performance fee', function () {
         });
       });
 
-      describe('price changing cases', function () {
+      describe.only('price changing cases', function () {
+        const feeRate = BigNumber.from(1000);
+        const valueOffset = ether('10');
+        beforeEach(async function () {
+          await pFeeModule.setPerformanceFeeRate(feeRate);
+          await pFeeModule.initializePerformanceFee();
+        });
+
         describe('among single period', function () {
+          const timeOffset = period.toNumber() * 0.4;
+
           describe('from low to high', function () {
-            it('hwm lower than the lower price', async function () {});
+            it('hwm lower than the lower price', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset.mul(2)));
+              await pFeeModule.updatePerformanceFee();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const pFeeAfter = await tokenS.balanceOf(outstandingAccount);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.gt(pFeeBefore);
+            });
 
-            it('hwm higher than the higher price', async function () {});
+            it('hwm higher than the higher price', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset.mul(2)));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const pFeeAfter = await tokenS.balanceOf(outstandingAccount);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.eq(pFeeBefore);
+              expect(pFeeAfter).to.be.eq(ether('0'));
+            });
 
-            it('hwm between two prices', async function () {});
+            it('hwm between two prices', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset.mul(2)));
+              await pFeeModule.updatePerformanceFee();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const pFeeAfter = await tokenS.balanceOf(outstandingAccount);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.gt(pFeeBefore);
+              expect(pFeeBefore).to.be.eq(ether('0'));
+            });
           });
 
           describe('from high to low', function () {
-            it('hwm lower than the lower price', async function () {});
+            it('hwm lower than the lower price', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset.mul(2)));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const pFeeAfter = await tokenS.balanceOf(outstandingAccount);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.lt(pFeeBefore);
+              expect(pFeeAfter).to.be.gt(ether('0'));
+            });
 
-            it('hwm higher than the higher price', async function () {});
+            it('hwm higher than the higher price', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset.mul(2)));
+              await pFeeModule.updatePerformanceFee();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const pFeeAfter = await tokenS.balanceOf(outstandingAccount);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.eq(pFeeBefore);
+              expect(pFeeAfter).to.be.eq(ether('0'));
+            });
 
-            it('hwm between two prices', async function () {});
+            it('hwm between two prices', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const pFeeAfter = await tokenS.balanceOf(outstandingAccount);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.lt(pFeeBefore);
+              expect(pFeeAfter).to.be.eq(ether('0'));
+            });
           });
         });
 
         describe('cross period', function () {
+          const timeOffset = period.toNumber() * 1.2;
+
           describe('from low to high', function () {
-            it('hwm lower than the lower price', async function () {});
+            it('hwm lower than the lower price', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              const ownerShareBefore = await tokenS.balanceOf(manager.address);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset.mul(2)));
+              await pFeeModule.crystallize();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const ownerShareAfter = await tokenS.balanceOf(manager.address);
+              const pFeeAfter = ownerShareAfter.sub(ownerShareBefore);
+              expect(hwmAfter).to.be.gt(hwmBefore);
+              expect(pFeeAfter).to.be.gt(pFeeBefore);
+            });
 
-            it('hwm higher than the higher price', async function () {});
+            it('hwm higher than the higher price', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset.mul(2)));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              const ownerShareBefore = await tokenS.balanceOf(manager.address);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset));
+              await pFeeModule.crystallize();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const ownerShareAfter = await tokenS.balanceOf(manager.address);
+              const pFeeAfter = ownerShareAfter.sub(ownerShareBefore);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.eq(pFeeBefore);
+              expect(pFeeAfter).to.be.eq(ether('0'));
+            });
 
-            it('hwm between two prices', async function () {});
+            it('hwm between two prices', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              const ownerShareBefore = await tokenS.balanceOf(manager.address);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset));
+              await pFeeModule.crystallize();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const ownerShareAfter = await tokenS.balanceOf(manager.address);
+              const pFeeAfter = ownerShareAfter.sub(ownerShareBefore);
+              expect(hwmAfter).to.be.gt(hwmBefore);
+              expect(pFeeAfter).to.be.gt(pFeeBefore);
+            });
           });
 
           describe('from high to low', function () {
-            it('hwm lower than the lower price', async function () {});
+            it('hwm lower than the lower price', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset.mul(2)));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              const ownerShareBefore = await tokenS.balanceOf(manager.address);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset));
+              await pFeeModule.crystallize();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const ownerShareAfter = await tokenS.balanceOf(manager.address);
+              const pFeeAfter = ownerShareAfter.sub(ownerShareBefore);
+              expect(hwmAfter).to.be.gt(hwmBefore);
+              expect(pFeeAfter).to.be.lt(pFeeBefore);
+            });
 
-            it('hwm higher than the higher price', async function () {});
+            it('hwm higher than the higher price', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              const ownerShareBefore = await tokenS.balanceOf(manager.address);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset.mul(2)));
+              await pFeeModule.crystallize();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const ownerShareAfter = await tokenS.balanceOf(manager.address);
+              const pFeeAfter = ownerShareAfter.sub(ownerShareBefore);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.eq(pFeeBefore);
+              expect(pFeeAfter).to.be.eq(ether('0'));
+            });
 
-            it('hwm between two prices', async function () {});
+            it('hwm between two prices', async function () {
+              // Set start price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.add(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              await increaseNextBlockTimeBy(timeOffset);
+              // Get start state
+              const hwmBefore = await pFeeModule.hwm64x64();
+              const pFeeBefore = await tokenS.balanceOf(outstandingAccount);
+              const ownerShareBefore = await tokenS.balanceOf(manager.address);
+              // Set after price
+              await pFeeModule.setGrossAssetValue(grossAssetValue.sub(valueOffset));
+              await pFeeModule.updatePerformanceFee();
+              // Get after state
+              const hwmAfter = await pFeeModule.hwm64x64();
+              const ownerShareAfter = await tokenS.balanceOf(manager.address);
+              const pFeeAfter = ownerShareAfter.sub(ownerShareBefore);
+              expect(hwmAfter).to.be.eq(hwmBefore);
+              expect(pFeeAfter).to.be.lt(pFeeBefore);
+              expect(pFeeAfter).to.be.eq(ether('0'));
+            });
           });
         });
       });
