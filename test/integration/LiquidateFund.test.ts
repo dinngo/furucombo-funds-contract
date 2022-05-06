200358fbd53b37e6b57684a7138347f451793efa
Albert Lin <albert@dinngo.co>
2022-05-06 18:08:14 +0800

    test: add some test cases to liquidate fund integration test

--- test/integration/LiquidateFund.test.ts
+++ test/integration/LiquidateFund.test.ts
@@ -1,4 +1,4 @@
-import { Wallet, Signer } from 'ethers';
+import { Wallet, Signer, BigNumber, constants } from 'ethers';
 import { deployments } from 'hardhat';
 import { expect } from 'chai';
 
@@ -11,15 +11,22 @@ import {
   AFurucombo,
   TaskExecutor,
   ShareToken,
-  MortgageVault,
   FundProxyFactory,
   HQuickSwap,
   Chainlink,
+  IUniswapV2Router02,
 } from '../../typechain';
 
-import { mwei, impersonateAndInjectEther, increaseNextBlockTimeBy } from '../utils/utils';
+import { mwei, impersonateAndInjectEther } from '../utils/utils';
 
-import { setPendingAssetFund, setExecutingDenominationFund, createReviewingFund, createFundInfra } from './fund';
+import {
+  setPendingAssetFund,
+  setExecutingDenominationFund,
+  createReviewingFund,
+  createFundInfra,
+  execSwap,
+  setLiquidatingAssetFund,
+} from './fund';
 import { deployFurucomboProxyAndRegistry, createFundProxy } from './deploy';
 import {
   BAT_TOKEN,
@@ -35,17 +42,10 @@ import {
   BAT_PROVIDER,
   MINIMUM_SHARE,
   FUND_PERCENTAGE_BASE,
+  QUICKSWAP_ROUTER,
 } from '../utils/constants';
 
 describe('LiquidateFund', function () {
-  let owner: Wallet;
-  let collector: Wallet;
-  let manager: Wallet;
-  let investor: Wallet;
-  let liquidator: Wallet;
-  let denominationProvider: Signer;
-  let mortgageProvider: Signer;
-
   const denominationProviderAddress = USDC_PROVIDER;
   const denominationAddress = USDC_TOKEN;
   const mortgageProviderAddress = BAT_PROVIDER;
@@ -72,8 +72,15 @@ describe('LiquidateFund', function () {
   const purchaseAmount = initialFunds;
   const swapAmount = purchaseAmount.div(2);
   const redeemAmount = purchaseAmount.sub(MINIMUM_SHARE);
-
   const shareTokenName = 'TEST';
+
+  let owner: Wallet;
+  let collector: Wallet;
+  let manager: Wallet;
+  let investor: Wallet;
+  let liquidator: Wallet;
+  let denominationProvider: Signer;
+  let mortgageProvider: Signer;
 
   let fRegistry: FurucomboRegistry;
   let furucombo: FurucomboProxy;
@@ -81,15 +88,17 @@ describe('LiquidateFund', function () {
   let fundProxyFactory: FundProxyFactory;
   let aFurucombo: AFurucombo;
   let taskExecutor: TaskExecutor;
-  let mortgageVault: MortgageVault;
   let oracle: Chainlink;
 
   let fundProxy: FundImplementation;
   let hQuickSwap: HQuickSwap;
 
   let denomination: IERC20;
+  let tokenA: IERC20;
   let mortgage: IERC20;
   let shareToken: ShareToken;
+
+  let quickRouter: IUniswapV2Router02;
 
   const setupFailTest = deployments.createFixture(async ({ deployments, ethers }, options) => {
     await deployments.fixture(''); // ensure you start from a fresh deployments
@@ -102,7 +111,7 @@ describe('LiquidateFund', function () {
     [fRegistry, furucombo] = await deployFurucomboProxyAndRegistry();
 
     // Deploy furucombo funds contracts
-    [fundProxy, , denomination, shareToken, taskExecutor, aFurucombo, hFunds, , , , , , hQuickSwap] =
+    [fundProxy, , denomination, shareToken, taskExecutor, aFurucombo, hFunds, , , oracle, , , hQuickSwap] =
       await createReviewingFund(
         owner,
         collector,
@@ -128,6 +137,9 @@ describe('LiquidateFund', function () {
         furucombo
       );
 
+    // External
+    quickRouter = await ethers.getContractAt('IUniswapV2Router02', QUICKSWAP_ROUTER);
+
     // Transfer token to investor
     await denomination.connect(denominationProvider).transfer(investor.address, initialFunds);
   });
@@ -145,40 +157,30 @@ describe('LiquidateFund', function () {
     [fRegistry, furucombo] = await deployFurucomboProxyAndRegistry();
 
     // Deploy furucombo funds contracts
-    [
-      fundProxyFactory,
-      taskExecutor,
-      aFurucombo,
-      hFunds,
-      denomination,
-      ,
-      ,
-      mortgage,
-      mortgageVault,
-      oracle,
-      ,
-      ,
-      hQuickSwap,
-    ] = await createFundInfra(
-      owner,
-      collector,
-      manager,
-      liquidator,
-      denominationAddress,
-      mortgageAddress,
-      tokenAAddress,
-      tokenBAddress,
-      denominationAggregator,
-      tokenAAggregator,
-      tokenBAggregator,
-      level,
-      stakeAmountS,
-      execFeePercentage,
-      pendingExpiration,
-      valueTolerance,
-      fRegistry,
-      furucombo
-    );
+    [fundProxyFactory, taskExecutor, aFurucombo, hFunds, denomination, tokenA, , mortgage, , oracle, , , hQuickSwap] =
+      await createFundInfra(
+        owner,
+        collector,
+        manager,
+        liquidator,
+        denominationAddress,
+        mortgageAddress,
+        tokenAAddress,
+        tokenBAddress,
+        denominationAggregator,
+        tokenAAggregator,
+        tokenBAggregator,
+        level,
+        stakeAmountS,
+        execFeePercentage,
+        pendingExpiration,
+        valueTolerance,
+        fRegistry,
+        furucombo
+      );
+
+    // External
+    quickRouter = await ethers.getContractAt('IUniswapV2Router02', QUICKSWAP_ROUTER);
 
     // Set oracle stale period
     await oracle.setStalePeriod(pendingExpiration * 2);
@@ -207,17 +209,20 @@ describe('LiquidateFund', function () {
     await mortgage.connect(manager).approve(fundProxy.address, stakeAmountS);
   });
 
-  describe('fail', function () {
+  describe('liquidate fail', function () {
     beforeEach(async function () {
       await setupFailTest();
     });
+
     it('should revert: in reviewing', async function () {
       await expect(fundProxy.liquidate()).to.be.revertedWith('RevertCode(10)'); // IMPLEMENTATION_PENDING_NOT_START
     });
+
     it('should revert: in executing', async function () {
       await fundProxy.connect(manager).finalize();
       await expect(fundProxy.liquidate()).to.be.revertedWith('RevertCode(10)'); // IMPLEMENTATION_PENDING_NOT_START
     });
+
     it('should revert: in pending expiration', async function () {
       await fundProxy.connect(manager).finalize();
 
@@ -240,22 +245,80 @@ describe('LiquidateFund', function () {
       );
       await expect(fundProxy.liquidate()).to.be.revertedWith('RevertCode(11)'); // IMPLEMENTATION_PENDING_NOT_EXPIRE
     });
+
     it('should revert: in closed', async function () {
       await fundProxy.connect(manager).finalize();
       await setExecutingDenominationFund(investor, fundProxy, denomination, shareToken, purchaseAmount);
       await fundProxy.connect(manager).close();
       await expect(fundProxy.liquidate()).to.be.revertedWith('RevertCode(10)'); // IMPLEMENTATION_PENDING_NOT_START
     });
+
+    it('should revert: in liquidating', async function () {
+      await oracle.setStalePeriod(pendingExpiration * 2);
+      await fundProxy.connect(manager).finalize();
+      await setLiquidatingAssetFund(
+        manager,
+        investor,
+        liquidator,
+        fundProxy,
+        denomination,
+        shareToken,
+        purchaseAmount,
+        swapAmount,
+        redeemAmount,
+        execFeePercentage,
+        denominationAddress,
+        tokenAAddress,
+        hFunds,
+        aFurucombo,
+        taskExecutor,
+        oracle,
+        hQuickSwap,
+        pendingExpiration
+      );
+
+      await expect(fundProxy.liquidate()).to.be.revertedWith('RevertCode(10)'); // IMPLEMENTATION_PENDING_NOT_START
+    });
   });
-  describe('success', function () {
+
+  describe('liquidate success', function () {
     beforeEach(async function () {
       await setupSuccessTest();
     });
-    it('in pending exceeds pending expiration', async function () {
+
+    it('in pending exceeds pending expiration by owner', async function () {
+      await fundProxy.connect(manager).finalize();
+      await setLiquidatingAssetFund(
+        manager,
+        investor,
+        manager,
+        fundProxy,
+        denomination,
+        shareToken,
+        purchaseAmount,
+        swapAmount,
+        redeemAmount,
+        execFeePercentage,
+        denominationAddress,
+        tokenAAddress,
+        hFunds,
+        aFurucombo,
+        taskExecutor,
+        oracle,
+        hQuickSwap,
+        pendingExpiration
+      );
+
+      expect(await fundProxy.state()).to.be.eq(FUND_STATE.LIQUIDATING);
+      expect(await fundProxy.owner()).to.be.eq(liquidator.address);
+    });
+
+    it('in pending exceeds pending expiration by investor', async function () {
       await fundProxy.connect(manager).finalize();
 
-      await setPendingAssetFund(
+      await setLiquidatingAssetFund(
         manager,
+        investor,
         investor,
         fundProxy,
         denomination,
@@ -269,13 +332,106 @@ describe('LiquidateFund', function () {
         hFunds,
         aFurucombo,
         taskExecutor,
-        hQuickSwap
+        oracle,
+        hQuickSwap,
+        pendingExpiration
       );
-      await increaseNextBlockTimeBy(pendingExpiration);
-      await fundProxy.liquidate();
 
       expect(await fundProxy.state()).to.be.eq(FUND_STATE.LIQUIDATING);
       expect(await fundProxy.owner()).to.be.eq(liquidator.address);
+    });
+  });
+
+  describe('operation in liquidating', function () {
+    beforeEach(async function () {
+      await setupSuccessTest();
+      await fundProxy.connect(manager).finalize();
+
+      await setLiquidatingAssetFund(
+        manager,
+        investor,
+        liquidator,
+        fundProxy,
+        denomination,
+        shareToken,
+        purchaseAmount,
+        swapAmount,
+        redeemAmount,
+        execFeePercentage,
+        denominationAddress,
+        tokenAAddress,
+        hFunds,
+        aFurucombo,
+        taskExecutor,
+        oracle,
+        hQuickSwap,
+        pendingExpiration
+      );
+    });
+
+    it('execute in liquidating', async function () {
+      const fundVault = await fundProxy.vault();
+      const amountIn = await tokenA.balanceOf(fundVault);
+      const actionAmountIn = amountIn
+        .mul(BigNumber.from(FUND_PERCENTAGE_BASE).sub(execFeePercentage))
+        .div(FUND_PERCENTAGE_BASE);
+      const path = [tokenA.address, denomination.address];
+      const tos = [hFunds.address, hQuickSwap.address];
+
+      // Get expect amount out
+      const amountOuts = await quickRouter.getAmountsOut(actionAmountIn, path);
+      const amountOut = amountOuts[amountOuts.length - 1];
+      const denominationBalanceBefore = await denomination.balanceOf(fundVault);
+
+      // Execute strategy in liquidating
+      await execSwap(
+        amountIn,
+        execFeePercentage,
+        tokenA.address,
+        denomination.address,
+        path,
+        tos,
+        aFurucombo,
+        taskExecutor,
+        fundProxy,
+        liquidator
+      );
+
+      // check denomination will decrease and token will increase
+      expect(await denomination.balanceOf(fundVault)).to.be.eq(denominationBalanceBefore.add(amountOut));
+    });
+
+    it('should revert: not allow to settle in liquidating', async function () {
+      const fundVault = await fundProxy.vault();
+      const amountIn = await tokenA.balanceOf(fundVault);
+      const actionAmountIn = amountIn
+        .mul(BigNumber.from(FUND_PERCENTAGE_BASE).sub(execFeePercentage))
+        .div(FUND_PERCENTAGE_BASE);
+      const path = [tokenA.address, denomination.address];
+      const tos = [hFunds.address, hQuickSwap.address];
+
+      // Get expect amount out
+      const amountOuts = await quickRouter.getAmountsOut(actionAmountIn, path);
+      const amountOut = amountOuts[amountOuts.length - 1];
+      const denominationBalanceBefore = await denomination.balanceOf(fundVault);
+
+      // Execute strategy in liquidating
+      await execSwap(
+        amountIn,
+        execFeePercentage,
+        tokenA.address,
+        denomination.address,
+        path,
+        tos,
+        aFurucombo,
+        taskExecutor,
+        fundProxy,
+        liquidator
+      );
+
+      // check denomination will decrease and token will increase
+      expect(await denomination.balanceOf(fundVault)).to.be.eq(denominationBalanceBefore.add(amountOut));
+      await expect(fundProxy.connect(liquidator).resume()).to.be.revertedWith('InvalidState(4)');
     });
   });
 });
